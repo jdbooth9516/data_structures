4ec72b722cfe3fbdf6d952cb06c225778fc71004
Jeremiah Booth <jeremiah.boothphotog@gmail.com>
2021-05-24 14:02:30 -0700

    added in order traverse

--- trees.py
+++ trees.py
@@ -62,16 +62,51 @@ class Tree:
                     return print("data was not found")
 
 
-            
+    def in_order_traverse(self):
+        #to traverse a tree of unknown size 
+
+        current = self.root
+        previous = None
+        end = False
+
+        while not end:
+            if current.left is not None:
+                previous = current
+                current = current.left
+
+            elif current == self.root and self.root.left == None and self.root.right != None and self.root.data != None:
+                print( self.root.data)
+                self.root.data = None
+
+                previous = current
+                current = current.right
+
+
+            elif self.root.left == None and self.root.right == None:
+                end = True
+
+            else: 
+                print(current.data)
+                if previous.left != None and previous.left.data == current.data:
+                    previous.left = None
+                    current = self.root
+                elif previous.right != None and previous.right.data == current.data:
+                    previous.right = None
+                    current = self.root
+
+
 tree = Tree()
+tree_traverse = tree
 
 tree.add_node(20)
 tree.add_node(25)
 tree.add_node(15)
 tree.add_node(17) 
-tree.add_node(23)
-tree.add_node(22)
-tree.add_node(7)
-tree.add_node(13)
 
-tree.search_tree(22)
+tree.add_node(7)
+
+
+tree.search_tree(17)
+
+tree_traverse.in_order_traverse()
+print(tree)
